6514f098fd8e20cd62b61a8fa292513b8f8fa800
Dtc-Xtreme <Dtc-Xtreme@CI9-10900K>
2023-09-08 00:26:31 +0200

    Add project files.

---
+++ PostToExternalAPI/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using InternalAuth.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+
+namespace InternalAuth.Controllers
+{
+    //[Authorize(Roles = "Admin")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            ViewBag.User = this.User?.Identity?.Name;
+            base.OnActionExecuting(context);
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FormViewModel form)
+        {
+            if (ModelState.IsValid)
+            {
+                // create json from form
+                string json = form.Parameters;
+                SendPostRequestExternal(json);
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private async void SendPostRequestExternal(string jsonData)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+
+                // API endpoint URL
+                string apiUrl = "https://wapi/v2.11.3/logout";
+
+                // Basic Authentication credentials (username and password)
+                string username = "your_username";
+                string password = "your_password";
+                string base64Credentials = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes($"{username}:{password}"));
+
+                // Set the content type header to JSON
+                httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+                httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64Credentials);
+
+                // Send the POST request and get the response
+                HttpResponseMessage response = await httpClient.PostAsync(apiUrl, new StringContent(jsonData));
+
+                // Check if the request was successful (status code 200)
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Response from the API:");
+                    Console.WriteLine(responseContent);
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ PostToExternalAPI/Models/FormViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InternalAuth.Models
+{
+    public class FormViewModel
+    {
+        [Required]
+        public string Parameters { get; set; }
+    }
+}
